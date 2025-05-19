468a37069c62aaac58d5ac57236f7dca83ec49d1
dispet <liftesk@gmail.com>
2025-05-19 23:34:52 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/test-task.elf.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/CLionProjects/test-task/Startup/startup_stm32f030f4px.s" "D:/CLionProjects/test-task/cmake-build-debug/CMakeFiles/test-task.elf.dir/Startup/startup_stm32f030f4px.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F030x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/CLionProjects/test-task/Inc"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Inc"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "D:/CLionProjects/test-task/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "D:/CLionProjects/test-task/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj.d"
+  "D:/CLionProjects/test-task/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj.d"
+  "D:/CLionProjects/test-task/Src/main.c" "CMakeFiles/test-task.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Src/main.c.obj.d"
+  "D:/CLionProjects/test-task/Src/stm32f0xx_hal_msp.c" "CMakeFiles/test-task.elf.dir/Src/stm32f0xx_hal_msp.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Src/stm32f0xx_hal_msp.c.obj.d"
+  "D:/CLionProjects/test-task/Src/stm32f0xx_it.c" "CMakeFiles/test-task.elf.dir/Src/stm32f0xx_it.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Src/stm32f0xx_it.c.obj.d"
+  "D:/CLionProjects/test-task/Src/syscalls.c" "CMakeFiles/test-task.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Src/syscalls.c.obj.d"
+  "D:/CLionProjects/test-task/Src/sysmem.c" "CMakeFiles/test-task.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Src/sysmem.c.obj.d"
+  "D:/CLionProjects/test-task/Src/system_stm32f0xx.c" "CMakeFiles/test-task.elf.dir/Src/system_stm32f0xx.c.obj" "gcc" "CMakeFiles/test-task.elf.dir/Src/system_stm32f0xx.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
